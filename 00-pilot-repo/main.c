7fa5a4fd7aeb31207097c5906fb1e55f8f865554
henryaudi <henryperfectest@gmail.com>
2026-03-01 23:01:50 -0700

    Add some stuff for 00

---
+++ 00-pilot-repo/main.c
@@ -0,0 +1,25 @@
+/**
+ * This is a simple C program that demonstrates basic arithmetic operations and output formatting.
+ * It calculates the sum of two integers and prints the result to the console.
+ *
+ * @author Henry
+ * @date 02/27/2026
+ * @version 1.0
+ */
+#include <stdio.h>
+
+int main()
+{
+  double x;
+  int a, b, c;
+
+  // We can assign values to the integers a, b, and c with = operator
+  a = 5;
+  b = 10;
+  c = 12;
+
+  scanf("%lf", &x);
+  printf("The value of x is: %.2lf\n", x);
+
+  return 0;
+}
